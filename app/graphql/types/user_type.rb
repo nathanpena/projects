9f546c636aed07053906ab1e8893a509140d9db7
nathanpena <nnpena@outlook.com>
2017-10-21 11:44:58 -0400

    Add basic graphql schema

---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,10 @@
+module Types
+	UserType = GraphQL::ObjectType.define do 
+		name "User"
+		description "a user"
+
+		field :id, !types.Int
+		field :emailAddress, !types.String, property: :email
+
+	end
+end
