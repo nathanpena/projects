9f0b9ff161cb71298f27b5ab7961306b97321bbc
nathanpena <nnpena@outlook.com>
2017-10-21 10:50:23 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,16 @@
 PROJECT_TITLES = 10.times.map { Faker::Internet.domain_word }
+
+SPLIT = 0.4
+OFFSET = PROJECT_TITLES.length * SPLIT
+
+PROJECT_USERS = {
+	'nathan@example.com' => PROJECT_TITLES[0, OFFSET],
+	'mary@example.com' => PROJECT_TITLES[OFFSET..-1]
+}
+
+PROJECT_USERS.each do |email, projects|
+	user = User.new(email: email, password: 'secret')
+	projects.each { |title| user.projects.build(title: title)}
+	user.save
+	puts "Created #{projects} for #{user.email}"
+end
